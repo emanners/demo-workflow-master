dda46c86678f7e47dec5ca5c2c645e74aa94f5a1
Ed Manners <ed.manners@gmail.com>
2025-05-05 16:55:49 +0100

    complete setup, start ci/cd

--- worker/src/main/java/solanceworkflowplatform/worker/WorkerApplication.java
+++ worker/src/main/java/solanceworkflowplatform/worker/WorkerApplication.java
@@ -2,12 +2,17 @@ package solanceworkflowplatform.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class WorkerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkerApplication.class, args);
+	}
+	@Scheduled(fixedDelay = Long.MAX_VALUE)
+	public void keepAlive() {
+		// no-op: just prevents the container from exiting
 	}
 
 }
